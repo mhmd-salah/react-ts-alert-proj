579900d8f1fd157b6f68bd5341323fb41e4248f6
mhmd-salah <ms.mhmdsalah@gmail.com>
2024-06-30 17:04:00 +0200

    pengin

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 
+import { Info } from 'lucide-react';
 import './App.css'
 import Alert from './components/Ui/Alert/Alert';
 
 function App() {
   return (
     <>
-    <Alert/>
+      <Alert
+        type={"alert-info"}
+        icon={<Info size={20} />}
+        title={"Note"}
+        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur temporibus doloremque praesentium
+        laboriosam iste totam officiis beatae sint non quas."
+      />
     </>
-  )
+  );
 }
 
 export default App
--- src/components/Ui/Alert/Alert.tsx
+++ src/components/Ui/Alert/Alert.tsx
@@ -1,18 +1,28 @@
-import { BellRing, X } from "lucide-react";
+import {  X } from "lucide-react";
 import "./index.scss";
-
-const Alert = () => {
+import { FC, ReactNode } from "react";
+interface Iprops {
+  type: string;
+  icon:ReactNode;
+  title:string;
+  description:string
+}
+const Alert: FC<Iprops> = ({ type ,icon,title,description}) => {
   return (
-    <div className="alert-wrapper">
-      <BellRing />
-      <h4>alert title </h4>
+    <div className={type}>
+      <div className="alert-header">
+        <div className="alert-title">
+          {icon}
+          <h4>{title} </h4>
+          
+        </div>
+        <span>
+          <X />
+        </span>
+      </div>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque fugit
-        laudantium provident atque nemo omnis, explicabo ad iure natus incidunt?
+        {description}
       </p>
-      <span>
-        <X/>
-      </span>
     </div>
   );
 };
